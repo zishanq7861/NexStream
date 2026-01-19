d034c9466e2c5f10c9e39f4f6851c0b8ea190343
Zishan Qureshi <zqureshi088@gmail.com>
2026-01-19 17:10:43 +0530

    Save current NexStream code

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv'
+dotenv.config({
+     path:"./.env"
+});
+
+import connectDB from "./db/dbConnection.js";
+
+import express from "express"
+const app = express();
+
+connectDB()
+.then(() => {
+    app.on("error",(error) => {
+        console.log("application not able to talk to database",error);
+        throw error;
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed!!!",err)
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,33 @@
+//using promises
+
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export { asyncHandler }
+
+//------------------------------------------------------------------------------------------------------
+
+//explanation of a function used in below function
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+
+
+//using try catch :)
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
